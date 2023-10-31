bcf55bbb7ba4ee5da5d82848cea6139830901004
HyunSu1768 <azxcv1768@gmail.com>
2023-10-31 16:19:28 +0900

    delete :: Delete DTO

--- src/main/kotlin/hs/kr/equus/storage/infrastructure/s3/dto/response/S3BulkResponseEntity.kt
+++
@@ -1,9 +0,0 @@
-package hs.kr.equus.storage.infrastructure.s3.dto.response
-
-data class S3BulkResponseEntity(
-    var bucket: String,
-    var fileKey: String,
-    var originFileName: String,
-    var successful: Boolean,
-    var statusCode: Int
-)
